8ba0a10a90a3db0176457250b42f77eafc33a99b
thanh1101681 <thanh1101681@gmail.com>
2016-05-19 14:27:34 +0700

    fix port 80 kill

--- config/aileeConfig.js
+++ config/aileeConfig.js
@@ -1,5 +1,5 @@
 module.exports.aileeConfig = {
-	baseUrlServer: 'http://aileephan.com:1337',
+	baseUrlServer: 'http://aileephan.com',
 	urlGetUserAccount: 'https://graph.facebook.com/v2.6/me?access_token=',
 	urlGetRoleApp: 'https://graph.facebook.com/v2.6/1032633966817570/roles?access_token=1032633966817570|59ec340e59efb1cd1df0568618060e62'
 };

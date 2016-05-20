99d9a1758671e504fba0595ffd834ab5b3c51bb5
thanh1101681 <thanh1101681@gmail.com>
2016-05-20 17:09:06 +0700

    fix localStorage

--- api/policies/isAdmin.js
+++ api/policies/isAdmin.js
@@ -1,6 +1,5 @@
 module.exports = function(req, res, next) {
     var requestify = require('requestify');
-    console.log('req.cookies', req.cookies);
     if (!_.isEmpty(req) &&
         !_.isEmpty(req.cookies) &&
         HelperService.CheckExistData(req.cookies.accessToken)) {

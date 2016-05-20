3afbe1f880f69e5956b5a13bde30f737b94e419f
thanh1101681 <thanh1101681@gmail.com>
2016-05-20 10:22:13 +0700

    fix lodash menu

--- assets/libs/menu/menu.js
+++ assets/libs/menu/menu.js
@@ -1,8 +1,7 @@
 define(function() {
     return {
         writeStories: function(uid) {
-            if (!_.isNull(uid) &&
-                !_.isUndefined(uid)) {
+            if (uid) {
                 window.location.href = window.location.origin + '/admin/write/' + uid;
             } else {
                 window.location.href = window.location.origin + '/admin/write';

a6689ef6b1df593999fb426c964c8122b5730257
thanh1101681 <thanh1101681@gmail.com>
2016-05-29 00:55:08 +0700

    prevent popups like facebook

--- assets/working/manage/main.js
+++ assets/working/manage/main.js
@@ -309,7 +309,7 @@ function onClickView(uid) {
                         onShow: function() {
                             setTimeout(function() {
                                 selfModal.modal('refresh');
-                            }, 200);
+                            }, 250);
                         }
                     });
                     selfModal.modal('show');

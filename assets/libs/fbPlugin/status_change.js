4acc0620b77dd4cc3b05287cd98bfa21d244afb3
thanh1101681 <thanh1101681@gmail.com>
2016-05-29 19:03:05 +0700

    fix all menu

--- assets/libs/fbPlugin/status_change.js
+++ assets/libs/fbPlugin/status_change.js
@@ -21,6 +21,7 @@ define(['./getFbUserProfile', './getFbAvatar', './getMenu'], function(getFbUserP
             var urlPicture = '/' + response.authResponse.userID + '/picture';
             Promise.all([getFbUserProfile(), getFbAvatar(urlPicture), getMenu(response)])
                 .then(function(successAll) {
+                    alert('after'+JSON.stringify(successAll[2].data))
                     setCookie('cookieMenu', JSON.stringify(successAll[2].data), 1);
 
                     function getCookie(cname) {

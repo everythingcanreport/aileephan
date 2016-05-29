fcc9ef4d6d036ed1d9f6aeb0db5485430237a142
thanh1101681 <thanh1101681@gmail.com>
2016-05-29 15:18:04 +0700

    fix login ios chrome

--- assets/working/home/main.js
+++ assets/working/home/main.js
@@ -1,3 +1,13 @@
+if (typeof localStorage === 'object') {
+    try {
+        localStorage.setItem('localStorage', 1);
+        localStorage.removeItem('localStorage');
+    } catch (e) {
+        Storage.prototype._setItem = Storage.prototype.setItem;
+        Storage.prototype.setItem = function() {};
+        alert('Your web browser does not support storing settings locally. In Safari, the most common cause of this is using "Private Browsing Mode". Some settings may not save or some features may not work properly for you.');
+    }
+}
 //check localStorageAvatar - localStorageProfile
 var localStorageAvatar = window.localStorage.getItem('localStorageAvatar');
 var localStorageProfile = window.localStorage.getItem('localStorageProfile');

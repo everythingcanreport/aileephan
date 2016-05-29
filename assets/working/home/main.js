88d56952707f17b87f4d046a2cd7d02ad6b1d958
thanh1101681 <thanh1101681@gmail.com>
2016-05-29 12:20:21 +0700

    fix login ios chrome

--- assets/working/home/main.js
+++ assets/working/home/main.js
@@ -125,7 +125,7 @@ $('window').on('touchmove', function(event) {
 function onClickLoginFacebook() {
     if (navigator.userAgent.match('CriOS'))
     window.open('https://www.facebook.com/dialog/oauth?client_id=1032633966817570&redirect_uri=' + document.location.href + '&scope=email,public_profile', '', function(response){
-        console.log('response', response);
+        alert('response', response);
     });
     else
         FB.login(null, { scope: 'email,public_profile' });

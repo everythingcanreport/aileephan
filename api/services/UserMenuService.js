f0c5da4797a826c1b0ea4e9f0763917c21055327
thanh1101681 <thanh1101681@gmail.com>
2016-05-29 16:27:11 +0700

    fix localstogare - cookies

--- api/services/UserMenuService.js
+++ api/services/UserMenuService.js
@@ -1,13 +1,13 @@
 module.exports = function(isAdmin) {
     if (isAdmin) {
         return [
-            { Name: 'Thêm mới truyện', icon: 'write', func: 'writeStories();' },
-            { Name: 'Quản lí truyện', icon: 'book', func: 'manageStories();' },
-            { Name: 'Thoát', icon: 'key', func: 'FB.logout();' }
+            { Name: 'Thêm mới truyện', icon: 'write', func: 'writeStories()' },
+            { Name: 'Quản lí truyện', icon: 'book', func: 'manageStories()' },
+            { Name: 'Thoát', icon: 'key', func: 'FB.logout()' }
         ];
     } else {
         return [
-            { Name: 'Thoát', icon: 'key', func: 'FB.logout();' }
+            { Name: 'Thoát', icon: 'key', func: 'FB.logout()' }
         ];
     }
 };

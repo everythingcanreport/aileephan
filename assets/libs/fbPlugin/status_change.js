45e59cae3f165376ad74b6f4953e7a430816680c
thanh1101681 <thanh1101681@gmail.com>
2016-05-30 01:09:51 +0700

    fix menu dropdown inline

--- assets/libs/fbPlugin/status_change.js
+++ assets/libs/fbPlugin/status_change.js
@@ -38,6 +38,7 @@ define(['./getFbUserProfile', './getFbAvatar', './getMenu'], function(getFbUserP
                     menus.forEach(function(menu, index) {
                         $('.connected-menu').append('<a class="item" onClick="' + menu.func + '"><i class="' + menu.icon + ' icon"></i>' + menu.Name + '</a>');
                     });
+                    $('.connected').dropdown('restore defaults');
                     $('.menu-loader').removeClass('active');
                     $('.connected').removeClass('hide');
                     $('.unknown').addClass('hide');

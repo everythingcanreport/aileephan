df2cce98c3088c333af97995444e60c54fca7b62
thanh1101681 <thanh1101681@gmail.com>
2016-05-29 14:59:34 +0700

    fix login ios chrome

--- assets/working/home/main.js
+++ assets/working/home/main.js
@@ -125,20 +125,7 @@ function onClickLoginFacebook() {
 //logout facebook
 function onClickLogoutFacebook() {
     FB.logout(function(response) {
-        //remove localStorage
-        window.localStorage.removeItem('localStorageAvatar');
-        window.localStorage.removeItem('localStorageMenu');
-        window.localStorage.removeItem('localStorageProfile');
-        //remove cookies
-        document.cookie = 'accessToken=;expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;';
-        //go home if not writing
-        if (window &&
-            window.location &&
-            window.location.pathname &&
-            window.location.pathname.indexOf('/admin/write') === -1 &&
-            window.location.pathname !== '/') {
-            window.location.href = window.location.origin;
-        }
+        FB.Auth.setAuthResponse(null, 'unknown');
     });
 };
 //end

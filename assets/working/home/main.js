98c5a89236879f667a777725db074543b424c80c
thanh1101681 <thanh1101681@gmail.com>
2016-05-29 14:02:50 +0700

    fix login ios chrome

--- assets/working/home/main.js
+++ assets/working/home/main.js
@@ -1,3 +1,20 @@
+//check param to call api get infomation - menu
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+console.log('no ne', getUrlParameter('code'));
+//ends
 //check localStorageAvatar - localStorageProfile
 var localStorageAvatar = window.localStorage.getItem('localStorageAvatar');
 var localStorageProfile = window.localStorage.getItem('localStorageProfile');
@@ -12,7 +29,6 @@ if (localStorageAvatar &&
     $('.unknown').addClass('hide');
 }
 //end
-console.log('vaooooo');
 //check localStorageMenu
 var localStorageMenu = window.localStorage.getItem('localStorageMenu');
 if (localStorageMenu) {

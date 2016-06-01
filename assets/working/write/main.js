74994e3a8b823dbb240a1e9dcf86163668d31a5b
thanh1101681 <thanh1101681@gmail.com>
2016-06-01 11:20:01 +0700

    fix css page error

--- assets/working/write/main.js
+++ assets/working/write/main.js
@@ -205,8 +205,8 @@ function onClickSave() {
         var htmlContent = tinymce.get('write-content').getContent();
         var textContent = tinymce.get('write-content').getContent({ format: 'text' });
         if (textContent &&
-            textContent.length > 200) {
-            var maxLengthCut = 200 // maximum number of characters to extract
+            textContent.length > 150) {
+            var maxLengthCut = 150 // maximum number of characters to extract
                 //trim the string to the maximum length
             var trimmedString = textContent.substr(0, maxLengthCut);
             //re-trim if we are in the middle of a word

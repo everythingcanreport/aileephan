af06a1b5328198179d37529895eeee83347aa1f5
thanh1101681 <thanh1101681@gmail.com>
2016-06-02 21:38:18 +0700

    minified mains

--- assets/working/error/main.js
+++ assets/working/error/main.js
@@ -103,3 +103,13 @@ function manageStories() {
         menu.manageStories();
     });
 };
+
+//login facebook
+function onClickLoginFacebook() {
+    if (navigator.userAgent.match('CriOS')) {
+        window.open('https://www.facebook.com/dialog/oauth?client_id=1032633966817570&redirect_uri=' + document.location.href + '&scope=email,public_profile&response_type=none', '', true);
+    } else {
+        FB.login(null, { scope: 'email,public_profile' });
+    }
+};
+//end

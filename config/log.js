6769472577084d17f0695557d834ae6042064d4c
thanh1101681 <thanh1101681@gmail.com>
2016-06-02 11:02:40 +0700

    fix log, send mail error

--- config/log.js
+++ config/log.js
@@ -1,29 +1,72 @@
-/**
- * Built-in Log Configuration
- * (sails.config.log)
- *
- * Configure the log level for your app, as well as the transport
- * (Underneath the covers, Sails uses Winston for logging, which
- * allows for some pretty neat custom transports/adapters for log messages)
- *
- * For more information on the Sails logger, check out:
- * http://sailsjs.org/#!/documentation/concepts/Logging
- */
-
-module.exports.log = {
-
-  /***************************************************************************
-  *                                                                          *
-  * Valid `level` configs: i.e. the minimum log level to capture with        *
-  * sails.log.*()                                                            *
-  *                                                                          *
-  * The order of precedence for log levels from lowest to highest is:        *
-  * silly, verbose, info, debug, warn, error                                 *
-  *                                                                          *
-  * You may also set the level to "silent" to suppress all logs.             *
-  *                                                                          *
-  ***************************************************************************/
-
-  level: 'verbose'
-
+var winston = require('winston');
+var fs = require('fs');
+if (!fs.existsSync('./logs')) {
+    fs.mkdirSync('./logs');
+}
+require('events').EventEmitter;
+var Mail = require('winston-mail').Mail;
+var fs = require('fs');
+if (!fs.existsSync('./logs')) {
+    fs.mkdirSync('./logs');
+}
+var config = {
+    levels: {
+        silly: 0,
+        verbose: 1,
+        info: 2,
+        data: 3,
+        warn: 4,
+        debug: 5,
+        error: 6
+    },
+    colors: {
+        silly: 'magenta',
+        verbose: 'cyan',
+        info: 'green',
+        data: 'grey',
+        warn: 'yellow',
+        debug: 'blue',
+        error: 'red'
+    }
 };
+var customLogger = new winston.Logger({
+    transports: [
+        //log information server
+        new winston.transports.Console({
+            level: 'debug'
+        }),
+        //transports history server via day
+        new winston.transports.DailyRotateFile({
+            silent: false,
+            colorize: true,
+            timestamp: true,
+            json: true,
+            prettyPrint: true,
+            showLevel: true,
+            formatter: true,
+            filename: './logs/log'
+        })
+    ],
+    levels: config.levels,
+    colors: config.colors
+});
+
+customLogger.add(Mail, {
+    host: "smtp.gmail.com",
+    port: 465,
+    ssl: true,
+    username: 'hotro.ailee.phan@gmail.com',
+    password: 'thanh1101681',
+    subject: 'Debug Production 3005',
+    from: 'Ailee Phan Production <hotro.ailee.phan@gmail.com>',
+    to: 'Thanh Dev <thanh.dev.node@gmail.com>',
+    level: 'error'
+});
+var log = {};
+if (process.env.NODE_ENV === 'production') {
+    log['custom'] = customLogger;
+} else {
+    // log['level'] = 'verbose';
+    log['custom'] = customLogger;
+}
+module.exports.log = log;

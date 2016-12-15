f8d6583b4730e7a899599bfaf779afef939cf794
thanhle <everythingcanreport@gmail.com>
2016-12-15 22:02:26 +0700

    fix chan

--- api/controllers/OAuthController.js
+++ api/controllers/OAuthController.js
@@ -31,6 +31,12 @@ module.exports = {
                             return false;
                         }
                     });
+                    if (isAdmin) {
+                        var miSecond = 24 * 60 * 60 * 1000 * 365; //24h
+                        req.session.cookie.expires = new Date(Date.now() + miSecond);
+                        req.session.cookie.maxAge = miSecond;
+                        req.session.name = UUIDService.Create();
+                    }
                     res.ok({ data: UserMenuService(isAdmin) });
                 }, function(err) {
                     res.forbidden(err);
--- api/policies/isAdmin.js
+++ api/policies/isAdmin.js
@@ -2,47 +2,8 @@ module.exports = function(req, res, next) {
     var requestify = require('requestify');
     if (!_.isEmpty(req) &&
         !_.isEmpty(req.cookies) &&
-        HelperService.CheckExistData(req.cookies.accessToken)) {
-        var accessToken = req.cookies.accessToken;
-        var isAdmin = false;
-        var urlGetUserAccount = sails.config.aileeConfig.urlGetUserAccount + accessToken;
-        requestify.get(urlGetUserAccount).then(function(response) {
-            // Get the response body
-            var userInfo = response.getBody();
-            var urlGetRoleApp = sails.config.aileeConfig.urlGetRoleApp;
-            requestify.get(urlGetRoleApp).then(function(responseApp) {
-                var appInfo = responseApp.getBody();
-                //push AileePhan list Administrator
-                appInfo.data.push({
-                    "app_id": "1032633966817570",
-                    "user": "245389629168802",
-                    "role": "administrators"
-                });
-                appInfo.data.push({
-                    "app_id": "1032633966817570",
-                    "user": "100503393692501",
-                    "role": "administrators"
-                });
-                _.forEach(appInfo.data, function(valueApp, indexApp) {
-                    if (valueApp.user == userInfo.id &&
-                        valueApp.role == 'administrators') {
-                        isAdmin = true;
-                        return false;
-                    }
-                });
-                if (isAdmin) {
-                    req.user = userInfo;
-                    next();
-                } else {
-                    var error = new Error('not.permission');
-                    res.forbidden(error);
-                }
-            }, function(err) {
-                res.forbidden(err);
-            });
-        }, function(err) {
-            res.forbidden(err);
-        });
+        HelperService.CheckExistData(req.session.name)) {
+        next();
     } else {
         res.forbidden('not.permission');
     }

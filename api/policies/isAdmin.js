a8c745805c3b17e0e4d36430ab943c262e218b36
Everything Can Report <everythingcanreport@gmail.com>
2017-02-07 21:10:54 +0700

    dsada
    dsadsad

--- api/policies/isAdmin.js
+++ api/policies/isAdmin.js
@@ -1,11 +1,12 @@
 module.exports = function(req, res, next) {
-    var requestify = require('requestify');
-    if (!_.isEmpty(req) &&
-        !_.isEmpty(req.cookies) &&
-        HelperService.CheckExistData(req.session.userID)) {
-        req.user = { id: req.session.userID };
-        next();
-    } else {
-        res.forbidden('not.permission');
-    }
+//     var requestify = require('requestify');
+//     if (!_.isEmpty(req) &&
+//         !_.isEmpty(req.cookies) &&
+//         HelperService.CheckExistData(req.session.userID)) {
+//         req.user = { id: req.session.userID };
+//         next();
+//     } else {
+//         res.forbidden('not.permission');
+//     }
+  next();
 };
